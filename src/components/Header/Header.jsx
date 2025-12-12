66ebf0a7824ef23d0a2de721963c39931d735d78
thebenstenator <thebenstenator@gmail.com>
2025-12-12 11:40:18 -0700

    Add font and style header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,19 @@
 import "./Header.css";
+import logo from "../../assets/logo.svg";
+import avatar from "../../assets/avatar.svg";
 
 function Header() {
-  return <header className="header">HEADER</header>;
+  return (
+    <header className="header">
+      <img src={logo} alt="logo" className="header__logo" />
+      <p className="header__date">Date, location</p>
+      <button className="header__add-clothes-btn">+ Add Clothes</button>
+      <div className="header__user-container">
+        <p className="header__username">Terrence Tegegne</p>
+        <img src={avatar} alt="Terrence Tegegne" className="header__avatar" />
+      </div>
+    </header>
+  );
 }
 
 export default Header;
