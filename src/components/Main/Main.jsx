1c5bb98f3de19657a1ecf5725d216544454f0ba5
thebenstenator <thebenstenator@gmail.com>
2025-12-12 13:59:45 -0700

    Add and style cards

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,11 +1,23 @@
 import WeatherCard from "../Weathercard/WeatherCard";
+import ItemCard from "../ItemCard/ItemCard";
+import { defaultClothingItems } from "../../utils/constants";
+import "./Main.css";
 
-function Main() {
+function Main({ weatherData }) {
   return (
     <main>
       <WeatherCard />
       <section className="cards">
         <p className="cards__text">Today is 75° F / You may want to wear:</p>
+        <ul className="cards__list">
+          {defaultClothingItems
+            .filter((item) => {
+              return item.weather === weatherData.type;
+            })
+            .map((item) => {
+              return <ItemCard key={item._id} item={item} />;
+            })}
+        </ul>
       </section>
     </main>
   );
